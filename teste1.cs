2e0395d4b5b121f8972656c69b32df1dfcfe5a15
IgorStarkH <igor.contato97@gmail.com>
2024-03-19 21:40:53 -0300

    first commit

---
+++ teste1.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int indice = 13, soma = 0, k = 0;
+
+        while (k < indice)
+        {
+            k += 1;
+            soma += k;
+        }
+
+        Console.WriteLine(soma);
+    }
+}
+
+//O Resultado final é: 91
